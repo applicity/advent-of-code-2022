db79cb8f94797eefacc3d0413d87748dac742f8d
Simon Matthews <sam@applicity.co.uk>
2022-12-02 12:15:51 +0000

    chore: Day 2 files

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 This repo has solutions to the AOC 2022 split by day.
 
 They are mostly in javascript
+
+One the second day of Christmas I switched to using TDD.  So it's now a project
+and an has some jest tests
---
+++ day02/p1/engine.js
@@ -0,0 +1,99 @@
+const scores = {
+  A: 1, // Rock
+  B: 2, // Paper
+  C: 3, // Scissors
+};
+
+const wins = {
+  "A:C": 1, // "Rock:Scissors": 1,
+  "C:B": 1, // "Scissors:Paper": 1,
+  "B:A": 1, //  "Paper:Rock": 1,
+};
+
+
+const toWin = {
+  A: 'B',
+  B: 'C',
+  C: 'A',
+}
+
+const toLoose = {
+  A: 'C',
+  B: 'A',
+  C: 'B',
+}
+
+
+
+const normaliseChoice = (a) => {
+  const map = {
+    X: "A",
+    Y: "B",
+    Z: "C",
+  };
+
+  return map[a];
+};
+
+const isWin = (a, b) => {
+  const result = [a, b].join(":");
+  if (wins[result]) return true;
+  return false;
+};
+
+const outcomeScore = (a, b) => {
+  let score = 0;
+
+  // Draw
+  if (a === b) {
+    score += 3;
+  } else if (isWin(b, a)) {
+    score += 6;
+  }
+
+  score += scores[b];
+
+  return score;
+};
+
+const scoreChoices = (choices) => {
+  return outcomeScore(choices[0], choices[1]);
+};
+
+const fixChoices = (a) => {
+  return [a[0], normaliseChoice(a[1])];
+};
+
+
+
+// X - Loose
+// Y - Draw
+// Z - Win
+const fixChoices2 = (a) => {
+
+  if (a[1] === 'Y') {
+    return [a[0], a[0]];
+  }
+
+  if (a[1] === 'X') {
+    return [a[0], toLoose[a[0]]];
+  }
+
+  if (a[1] === 'Z') {
+    return [a[0], toWin[a[0]]];
+  }
+
+};
+
+const rps = (input, p2) => {
+  const res = input
+    .split("\n")
+    .filter(Boolean)
+    .map((v) => v.split(" "))
+    .map(p2 ? fixChoices2 : fixChoices)
+    .map(scoreChoices)
+    .reduce((a, c) => a + c, 0);
+  return res;
+};
+
+module.exports =  rps;
